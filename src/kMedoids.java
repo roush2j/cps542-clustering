c9617dc101f6ab0ec23f20bf32183e2c8af3555d
JRoush <@>
2016-04-23 18:32:18 -0400

    Initial rewrite of kmedoids

--- src/kMedoids.java
+++ src/kMedoids.java
@@ -1,223 +1,176 @@
+import java.util.Arrays;
+import java.util.Random;
 
+public class kMedoids extends Clustering {
 
-public class kMedoids extends Clustering
-{
-	int temp;
-	int temp1;
+    private final int      clusterCnt;
 
-	//orignal Data set
-	private DataSet originalData;
-	//holds values of potential medoids
-	private double[] medoids;
-	//need for determining how many clusters to calculate
-	private int numClusters;
-	//need for determining max interations to run
-	private int maxiters;
-	//required to store the cost of using a point as a medoid
-	private double[][] cost;
-	/*
-	 * required to determine the min cost value of using
-	 * a particular point as a medoid
-	 */
-	private double costValue = Integer.MAX_VALUE;
-	//holds the data points of the best centers
-	private double[] bestCenters;
-	/**
-     * the algorithm implements the PAM algorithm
-     * it takes in data and a number that describes
-     * the number of clusters to find, the max number of interations, and the
-     * initial medoids to work with
-     * uses the first 'X' number of data points off the dataset variable, where
-     * 'X' is equal to the number of clusters desired
-     */
-	public kMedoids(DataSet data, int numClusters) 
-	{
-		super(data);
-		temp = data.attrCnt;
-		temp1 = data.tupleCnt;
-		originalData = data;
-		this.numClusters = numClusters;
-		maxiters = data.tupleCnt;
-		//initialize variables
-		cost = new double[numClusters][data.tupleCnt];
-		bestCenters = new double[numClusters * data.attrCnt];
-		//capture points to use as the initial medoid objects
-		medoids = new double[numClusters * data.attrCnt];
-		for(int i=0; i<numClusters; i++)
-		{
-			for(int j=0; j<data.attrCnt; j++)
-			{
-				medoids[i*data.attrCnt+j] = data.getAttr(i, j);
-			}
-		}
-		//run implementation of PAM algorithm
-		pamKmedoids(medoids);
+    private final int[]    _medoids;
 
-	}
+    private final double[] _tmperrs;
 
-	private void pamKmedoids(double[] medoids)
-	{
-		//current cost to assign a point to a cluster
-		double currentPointCost;
-		//previous run's value
-		double previousRunCost = -1;
-		//current run's value
-		double currentRunCost;
-		/*
-		 * tuple pointer, for use with capturing 
-		 * next tuple to use as medoid
-		 */
-		int pointer = 1;
-		//run for max iterations
-		for(int max=0; max<maxiters; max++)
-		{
-			currentRunCost = 0;
-			//for each cluster point
-			for(int i=0; i<numClusters; i++)
-			{
-				//for each original data point
-				for(int j=0; j<originalData.tupleCnt; j++)
-				{
-					//calculate the cost of using that as a medoid
-					for(int x=0; x<originalData.attrCnt; x++)
-					{
-						cost[i][j] += euclideanDist(originalData.getAttr(j, x), medoids[i*originalData.attrCnt+x]);
-					}
-				}
-			}
-			/*
-			 * determine the best medoid to use
-			 * note: best medoid to assign a point 
-			 * to is the one with the least cost 
-			 */
-			for(int j=0; j<originalData.tupleCnt; j++)
-			{
-				/*
-				 * re-initialize currentRunCost for 
-				 * each new column for the cost array
-				 */
-				currentPointCost = Integer.MAX_VALUE;
-				//determine which cluster the points belong to
-				for(int i=0; i<numClusters; i++)
-				{
-					if(cost[i][j] < currentPointCost)
-					{
-						currentPointCost = cost[i][j];
-					}
-				}
-				//sum the total cost using medoid as cluster center
-				currentRunCost += currentPointCost;
-			}
-			/*
-			 * determine if current cost is better, than previous
-			 * run's cost
-			 * if it is better, assign best centers, the initial medoid values
-			 * and assign currentRunCost to previousRunCost
-			 */
-			if(previousRunCost == -1)
-			{
-				for(int i=0; i<medoids.length; i++)
-				{
-					bestCenters[i] = medoids[i];
-				}
-				previousRunCost = currentRunCost;
-			}
-			else if(currentRunCost < previousRunCost)
-			{
-				for(int i=0; i<medoids.length; i++)
-				{
-					bestCenters[i] = medoids[i];
-				}
-				previousRunCost = currentRunCost;
-			}
-			/*
-			 * determine next points to use as the medoid
-			 */
-			if(originalData.tupleCnt - pointer >= numClusters)
-			{
-				for(int i=0; i<numClusters; i++)
-				{
-					for(int j=0; j<originalData.attrCnt; j++)
-					{
-						//medoids now holds the next set of points to test
-						medoids[i*originalData.attrCnt+j] = originalData.getAttr(i + pointer, j);
-					}
-				}
-			}
-			else
-			{
-				int tempPointer = pointer;
-				int captured = 0;
-				for(int i=0; i<numClusters; i++)
-				{
-					for(int j=0; j<originalData.attrCnt; j++)
-					{
-						if(i+tempPointer < originalData.tupleCnt)
-						{
-							medoids[i*originalData.attrCnt+j] = originalData.getAttr(i + tempPointer, j);
-						}
-						else if(i+tempPointer == originalData.tupleCnt)
-						{
-							tempPointer = 0 - captured;
-							medoids[i*originalData.attrCnt+j] = originalData.getAttr(i + tempPointer, j);
-						}
-						else
-						{
-							medoids[i*originalData.attrCnt+j] = originalData.getAttr(i + tempPointer, j);
-						}
-					}
-					captured ++;
-				}
-			}
-			//move the pointer to be ready for the next iteration
-			pointer++;			
-		}
-	}
-	
-	private double euclideanDist(double x1, double x2)
-	{
-		return (x1 - x2) * (x1 - x2);
-	}
-	
-	@Override
-	public int clusterCount() 
-	{
-		return numClusters;
-	}
+    private final int[]    mapping;
 
-	@Override
-	public int clusterID(int tuple) 
-	{
-		double minHere = Integer.MAX_VALUE;
-		int ID = -1;
-		for(int i=0; i<numClusters; i++)
-		{
-			if(cost[i][tuple] < minHere)
-			{
-				ID = i;
-			}
-		}
-		return ID + 1;
-	}
-	
-	public void printClusterCenters()
-	{
-		System.out.println("Cluster centers are: ");
-		for(int j=0; j<numClusters; j++)
-		{
-			for(int i=0; i<originalData.attrCnt; i++)
-			{
-				System.out.print(bestCenters[j * originalData.attrCnt + i] + ", ");
-			}
-			System.out.println();
-		}
-	}
+    private final int[]    _mappingx1, _mappingx2;
+
+    private final String   desc;
+
+    public kMedoids(DataSet data, int clusterCnt, int[] centers) {
+        super(data);
+        final int clusc = this.clusterCnt = clusterCnt, tupc = data.tupleCnt;
+        assert (centers.length >= clusc);
+        this.desc = String.format("%s(clusterCnt=%d)", getClass()
+                .getSimpleName(), clusc);
+
+        final int[] medoids = this._medoids = Arrays.copyOf(centers, clusc);
+        final int[] mapping = this.mapping = new int[tupc];
+        this._tmperrs = new double[clusc];
+        this._mappingx1 = new int[tupc];
+        this._mappingx2 = new int[tupc];
+
+        // initialize mappings
+        for (int t = 0; t < tupc; t++) {
+            double minD2 = Double.POSITIVE_INFINITY;
+            for (int c = 0; c < clusc; c++) {
+                double d2 = data.distSq(t, medoids[c]);
+                if (d2 >= minD2) continue;
+                minD2 = d2;
+                mapping[t] = c;
+            }
+        }
+
+        // For each iteration, we find a cluster c and non-medoid q such that
+        // replacing medoids[c] -> q yields the greatest reduction in total error.
+        while (true) {
+            int swapq = -1, swapc = -1;
+            double swapErr = 0;
+            int[] swapmap = this._mappingx1;
+            double[] tmperrs = this._tmperrs;
+            int[] tmpmap = this._mappingx2;
+
+            // loop over each non-medoid tuple and find the one with
+            // largest benefit when swapping with one of the medoids
+            for (int q = 0; q < tupc; q++) {
+
+                // reject q if it is already a medoid
+                boolean isMedoid = false;
+                for (int c = 0; !isMedoid && c < clusc; c++) {
+                    isMedoid = (medoids[c] == q);
+                }
+                if (isMedoid) continue;
+
+                // loop over each tuple to calculate the benefit
+                // of swapping medoids[c] -> q for each cluster c
+                //  tmperrs[c]  is running sum of change-in-err
+                //  tmpmap[t]   is a code for the new cluster ID (see below)
+                Arrays.fill(tmperrs, 0);
+                for (int t = 0; t < tupc; t++) {
+                    int closest = mapping[t];
+                    double closestD2 = data.distSq(t, medoids[closest]);
+                    double swapD2 = data.distSq(t, q);
+                    if (swapD2 < closestD2) {
+
+                        // q is closer than current closest medoid
+                        // swapping q<->c will reduce the error for *any* c
+                        double derr = Math.sqrt(swapD2) - Math.sqrt(closestD2);
+                        for (int c = 0; c < clusc; c++) {
+                            tmperrs[c] += derr;
+                        }
+                        // new clusterID is whatever q gets swapped with 
+                        tmpmap[t] = -1;
+
+                    } else {
+
+                        // q is farther than current closest medoid
+                        // find second-closest medoid
+                        int secondClosest = -1;
+                        double secondClosestD2 = Double.POSITIVE_INFINITY;
+                        for (int c = 0; c < clusc; c++) {
+                            if (c == closest) continue;
+                            double d2 = data.distSq(t, medoids[c]);
+                            if (d2 >= secondClosestD2) continue;
+                            secondClosestD2 = d2;
+                            secondClosest = c;
+                        }
+                        // error will be increased if we swap q <-> closest medoid
+                        double d = Math.sqrt(closestD2);
+                        if (swapD2 < secondClosestD2) {
+                            // q is closer than second closest medoid
+                            // new clusterID is always same as old cluster ID
+                            tmperrs[closest] += Math.sqrt(swapD2) - d;
+                            tmpmap[t] = closest;
+                        } else {
+                            // q is farther than second closest medoid
+                            // new clusterID is old cluster ID or second-closest
+                            // cluster ID, depending on what q replaces
+                            tmperrs[closest] += Math.sqrt(secondClosestD2) - d;
+                            tmpmap[t] = secondClosest;
+                        }
+                    }
+                }
+
+                // check if there is any current cluster c such that
+                // swapping medoids[c] -> q beats the current best swap
+                for (int c = 0; c < clusc; c++) {
+                    if (tmperrs[c] >= swapErr) continue;
+                    swapErr = tmperrs[c];
+                    swapq = q;
+                    swapc = c;
+                    int[] t = swapmap;
+                    swapmap = tmpmap;
+                    tmpmap = t;
+                }
+            }
+
+            // perform swap
+            if (swapErr >= 0) {
+                // If no swap is found that reduces total error, 
+                // the algorithm has terminated
+                break;
+            }
+            medoids[swapc] = swapq;
+
+            // update mapping
+            for (int t = 0; t < tupc; t++) {
+                if (swapmap[t] < 0) {
+                    // closest medoid is now q
+                    mapping[t] = swapc;
+                } else if (swapc == mapping[t]) {
+                    // we've replaced previous closest medoid
+                    mapping[t] = swapmap[t];
+                } else {
+                    // no change in closest medoid
+                }
+            }
+        }
+    }
+
+    public kMedoids(DataSet data, int clusterCnt, Random rand) {
+        this(data, clusterCnt, initCenters(data, clusterCnt, rand));
+    }
+
+    /** Find random starting points for clusters */
+    public static int[] initCenters(DataSet data, int clCnt, Random rand) {
+        int[] centers = new int[clCnt];
+        for (int c = 0; c < clCnt; c++) {
+            centers[c] = rand.nextInt(data.tupleCnt);
+        }
+        return centers;
+    }
 
     @Override public int tupleCount() {
-        return cost[0].length;
+        return mapping.length;
+    }
+
+    @Override public int clusterCount() {
+        return clusterCnt;
+    }
+
+    @Override public int clusterID(int tuple) {
+        return mapping[tuple] + 1;
     }
 
     @Override public String toString() {
-        return "KMedoids";
+        return desc;
     }
 }
